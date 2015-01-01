9cdb535714ce40f0ebe9ceb729e4192d5cd39dd8
James Hollowell <jamesthollowell@gmail.com>
2015-01-01 01:51:36 -0500

    HAPPY NEW YEAR!!!
    Update all the copyrights...

--- src/main/java/allout58/mods/techtree/commands/ResearchCommand.java
+++ src/main/java/allout58/mods/techtree/commands/ResearchCommand.java
@@ -1,7 +1,7 @@
 /***********************************************************************************
  * The MIT License (MIT)                                                           *
  *                                                                                 *
- * Copyright (c) 2014 allout58                                                     *
+ * Copyright (c) 2015 allout58                                                     *
  *                                                                                 *
  * Permission is hereby granted, free of charge, to any person obtaining a copy    *
  * of this software and associated documentation files (the "Software"), to deal   *
